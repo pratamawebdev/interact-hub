1b08dece1d5fc603c248fea1b343ebda26f2eb38
willy <willypratama231@gmail.com>
2024-05-10 23:45:10 +0800

    fix: fix type in Title component

--- components/elements/global/Title/index.tsx
+++ components/elements/global/Title/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Title = ({ children }) => {
+const Title = ({ children }: { children: React.ReactNode }) => {
   return (
     <h1 className="text-center font-bold text-black text-xl">{children}</h1>
   );
